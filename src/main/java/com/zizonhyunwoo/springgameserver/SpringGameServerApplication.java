966494ea213359c5b2d8f8059ee12228e3f73507
hyunwoo <daersh@naver.com>
2026-04-27 14:09:14 +0900

    init: 프로젝트 초기화

---
+++ src/main/java/com/zizonhyunwoo/springgameserver/SpringGameServerApplication.java
@@ -0,0 +1,13 @@
+package com.zizonhyunwoo.springgameserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGameServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringGameServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zizonhyunwoo/springgameserver/SpringGameServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zizonhyunwoo.springgameserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGameServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
